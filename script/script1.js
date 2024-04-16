bd6bb327b26b73f5b9081dcfa1893896059d8c0c
unknown <chapi.kaica@gmail.com>
2024-04-16 17:11:57 +0300

    Initial commit

---
+++ script/script1.js
@@ -0,0 +1,427 @@
+
+let gold = 10;
+let live = 50;
+let wepons1 = "";
+let wepons2 = "";
+let victory = 0;
+let beastWepon = '';
+let beastLive = 0;
+let counter = 0;
+let dragonLive = 400;
+var diamonds = 0;
+let counterFight = 0;
+
+// данните от магазин , битка, мина , битка с дракон
+const magazin = document.querySelector(".magazin");
+const mine = document.querySelector(".mine");
+const fight = document.querySelector(".fight");
+const killDragon = document.querySelector(".kill-dragon");
+const winPic = document.querySelector(".win-pic");
+const uvod = document.querySelector(".uvod");
+const game = document.getElementById("container");
+
+//ресурсите
+function updateResurs() {
+    document.querySelector(".diamant").textContent = diamonds
+    document.querySelector(".gold").textContent = gold;
+    document.querySelector(".victory").textContent = victory;
+    document.querySelector(".live").textContent = live;
+    document.querySelector(".weapon1").textContent = wepons1;
+    document.querySelector(".weapon2").textContent = wepons2;
+    document.querySelector(".animal").textContent = beastLive;
+    document.querySelector(".animal-wepon").textContent = beastWepon;
+
+};
+updateResurs();
+
+//видимост на събитията
+function styleDisplay(winPicVisible, magazinVisible, mineVisible, fightVisible, killDragonVisible, uvodVisible, gameVisible) {
+
+    winPic.style.display = winPicVisible ? "block" : "none";
+    magazin.style.display = magazinVisible ? "block" : "none";
+    mine.style.display = mineVisible ? "block" : "none";
+    fight.style.display = fightVisible ? "block" : "none";
+    killDragon.style.display = killDragonVisible ? "block" : "none";
+    uvod.style.display = uvodVisible ? 'block' : "none";
+    game.style.visibility = gameVisible ? "visible" : "hidden";
+}
+
+
+
+//функция за  вход в магазина
+document.getElementById('button1').addEventListener("click", function () {
+    cardVisibility();
+    selectedGold = 0;
+    let hours = new Date().getHours();
+    if (hours < 8 || hours > 20) {
+        alert("Работното време на магазина е от 8 h сутрин до 20 h вечер");
+        styleDisplay(false, false, false, false, false, false, false);
+    } else {
+        updateResurs();
+        styleDisplay(false, true, false, false, false, false, false);
+        danger();
+    }
+
+});
+//функция за покупка на живот 
+document.querySelector(".by-live").addEventListener("click", function () {
+
+    if (gold < 40) {
+        alert("Нямате достатъчно злато за тази покупка");
+    } else {
+        live += 20;
+        gold -= 40;
+        updateResurs();
+    };
+
+    danger();
+});
+//функция за покупка на оръжие
+document.getElementById('by-wepon').addEventListener("click", function () {
+    const choise = document.querySelector(".wepon");
+    const select = choise.value;
+
+    function buyWeapon(weapon, price) {
+        if (wepons1 === "") {
+            if (gold >= price) {
+                gold -= price;
+                wepons1 += weapon + " ";
+            } else {
+                alert("Нямате достатъчно злато за да купите това оръжие");
+            }
+        } else if (wepons2 === "") {
+            if (gold >= price) {
+                gold -= price;
+                wepons2 += weapon + " ";
+            } else {
+                alert("Нямате достатъчно злато за да купите това оръжие");
+            }
+        } else {
+            alert("Имате право само на 2 оръжия");
+        }
+    }
+
+    switch (select) {
+        case "sword":
+            buyWeapon("Меч", 400);
+            break;
+        case "shield":
+            buyWeapon("Щит", 500);
+            break;
+        case "armor":
+            buyWeapon("Броня", 880);
+            break;
+    }
+    danger();
+    updateResurs();
+});
+
+// функция за вход в  мината
+document.getElementById('button2').addEventListener("click", function () {
+    cardVisibility();
+    selectedGold = 0;
+    let hours = new Date().getHours();
+    if (hours < 8 || hours > 20) {
+        alert("Не може да копаеш когато мината не работи.Работно време от 8h до 20h")
+        styleDisplay(false, false, false, false, false, false, false);
+    } else {
+        danger();
+        updateResurs();
+        styleDisplay(false, false, true, false, false, false, false);
+        document.querySelector(".dril-result").textContent = "";
+    }
+});
+//функция за копаене в мината
+function deep() {
+
+    if (live <= 10) {
+        document.body.innerHTML = `<h1 style="text-align: center;">GAME OVER</h1> `
+        //  auto(deep);
+    } else {
+        live -= 10;
+        let result = Math.ceil(Math.random() * 100);
+        gold += result;
+        document.querySelector(".dril-result").textContent = `Печелите ${result} злато! - Губите 10 живот!`
+
+        updateResurs();
+
+    }
+    danger();
+}
+
+//функция за вход в битка
+document.getElementById('button3').addEventListener("click", function () {
+    cardVisibility();
+    selectedGold = 0;
+    let entryButton1 = document.getElementById("button1");
+    entryButton1.disabled = true;
+    let entrybuttno2 = document.getElementById("button2");
+    entrybuttno2.disabled = true;
+    let entrybuttno3 = document.getElementById("button3");
+    entrybuttno3.disabled = true;
+    let bardak = document.querySelector(".bardak");
+    bardak.disabled = true;
+    styleDisplay(false, false, false, true, false, false, false);
+
+    beastLive = Math.ceil(Math.random() * 300);
+    let curentWepon = Math.random();
+    if (curentWepon <= 1 / 3) {
+        beastWepon = "Меч";
+    } else if (curentWepon > 1 / 3 && curentWepon <= 2 / 3) {
+        beastWepon = "Щит";
+    } else {
+        beastWepon = "Броня"
+    };
+    const combatButton = document.getElementById('combatButton');
+    const runButton = document.getElementById('runButton');
+    combatButton.disabled = false;
+    runButton.disabled = false;
+    document.querySelector(".result").innerHTML = "";
+    updateResurs();
+
+})
+//функция за бягство от битка
+function escape() {
+
+    let answer = confirm("Сигурен ли си ,че искаш да избягаш? Ще изгубиш ресурси");
+    if (answer) {
+        if (diamonds > 0) {
+            diamonds--;
+        }
+        gold = Math.floor(gold /= 2);
+        let loseWepon = Math.random()
+        if (wepons1 === "" && wepons2 === "") {
+            gold = 0;
+        } else {
+            if (loseWepon <= 1 / 2) {
+                wepons1 = "";
+            } else {
+                if (wepons2 === "") {
+                    wepons1 = "";
+                }
+                wepons2 = "";
+            }
+        }
+        killDragon.style.display = "none";
+        fight.style.display = "none";
+        let entryButton1 = document.getElementById("button1");
+        entryButton1.disabled = false;
+        let entrybuttno2 = document.getElementById("button2");
+        entrybuttno2.disabled = false;
+        let entrybuttno3 = document.getElementById("button3");
+        entrybuttno3.disabled = false;
+        let bardak = document.querySelector(".bardak");
+        bardak.disabled = false;
+    } else {
+    }
+    const combatButton = document.getElementById('combatButton');
+    const runButton = document.getElementById('runButton');
+    combatButton.disabled = false;
+    runButton.disabled = false;
+    updateResurs();
+}
+//Функция за начало на битката
+document.querySelector(".combat").addEventListener("click", function () {
+    counterFight++;
+    if (live > beastLive && wepons1 !== "") {
+
+        if (beastWepon === "Щит") {
+            beastLive -= Math.ceil(Math.random() * 20);
+        } else if (beastWepon === "Броня") {
+            beastLive -= Math.ceil(Math.random() * 10);
+        } else {
+            beastLive -= Math.ceil(Math.random() * 50);
+        };
+
+        if (wepons2 === "Броня" || wepons1 === "Броня") {
+            live -= Math.ceil(Math.random() * 10)
+        } else {
+            live -= Math.ceil(Math.random() * 30)
+        };
+
+
+        if (beastLive <= 0) {
+            victoryAgenstBeast();
+        } else {
+            updateResurs();
+        };
+        if (live <= 0) {
+            document.body.innerHTML = '<h1 style="text-align: center;">GAME OVER</h1>'
+        } else if (live <= 100) {
+            document.querySelector(".result").innerHTML = "!!! ОПАСНОСТ ОТ ЗАГУБА !!!";
+        };
+
+    } else if (live >= beastLive) {
+
+        if (beastWepon === "Щит") {
+            beastLive -= Math.ceil(Math.random() * 30);
+        } else if (beastWepon === "Броня") {
+            beastLive -= Math.ceil(Math.random() * 10);
+        } else {
+            beastLive -= Math.ceil(Math.random() * 50);
+        };
+
+        if (wepons2 === "Броня" || wepons1 === "Броня") {
+            live -= Math.ceil(Math.random() * 15)
+        } else {
+            live -= Math.ceil(Math.random() * 40)
+        };
+
+        if (beastLive <= 0) {
+            victoryAgenstBeast();
+
+        } else {
+            updateResurs();
+        };
+        if (live <= 0) {
+            document.body.innerHTML = '<h1 style="text-align: center;">GAME OVER</h1>'
+        } else if (live <= 100) {
+            document.querySelector(".result").innerHTML = "!!! ОПАСНОСТ ОТ ЗАГУБА !!!";
+        };
+    } else {
+        if (beastWepon === "Щит") {
+            beastLive -= Math.ceil(Math.random() * 20);
+        } else if (beastWepon === "Броня") {
+            beastLive -= Math.ceil(Math.random() * 10);
+        } else {
+            beastLive -= Math.ceil(Math.random() * 30);
+        };
+
+        if (wepons2 === "Броня" || wepons1 === "Броня") {
+            live -= Math.ceil(Math.random() * 10)
+        } else {
+            live -= Math.ceil(Math.random() * 100)
+        };
+
+        if (beastLive <= 0) {
+            victoryAgenstBeast();
+
+        } else {
+            updateResurs();
+        };
+        if (live <= 0) {
+            document.body.innerHTML = '<h1 style="text-align: center;">GAME OVER</h1>'
+        } else if (live <= 100) {
+            document.querySelector(".result").innerHTML = "!!! ОПАСНОСТ ОТ ЗАГУБА !!!";
+        };
+    }
+    danger();
+
+
+})
+
+//Вход в Битка с дракона
+document.getElementById('button4').addEventListener("click", function () {
+    cardVisibility();
+    selectedGold = 0
+    dragonLive = 400;
+    if (counter > 0 && counter <= 2) {
+        dragonLive += 150;
+    } else if (counter === 3) {
+        dragonLive += 250
+    } else if (counter > 3) {
+        dragonLive += 350
+    }
+    counter++;
+    document.querySelector(".dragonLive").textContent = dragonLive;
+    updateResurs();
+    if (victory >= 10) {
+        styleDisplay(false, false, false, false, true, false, false);
+        let entryButton1 = document.getElementById("button1");
+        entryButton1.disabled = true;
+        let entrybuttno2 = document.getElementById("button2");
+        entrybuttno2.disabled = true;
+        let entrybuttno3 = document.getElementById("button3");
+        entrybuttno3.disabled = true;
+        let escapeButton = document.querySelector(".run");
+        escapeButton.disabled = false;
+        let escapeButton1 = document.getElementById("runButton");
+        escapeButton1.disabled = false;
+
+    } else {
+        alert("Трябва да победиш поне 10 звяра за да се биеш с дракона");
+    }
+    danger();
+
+})
+
+//Бой с дракона
+function combatDragon() {
+
+    if (wepons1 !== "" && wepons2 !== "") {
+        live -= Math.ceil(Math.random() * 25);
+        dragonLive -= Math.ceil(Math.random() * 15);
+        document.querySelector(".dragonLive").textContent = dragonLive;
+    }
+    if (wepons1 === "Броня" || wepons2 === "Броня") {
+        live -= Math.ceil(Math.random() * 20);
+        dragonLive -= Math.ceil(Math.random() * 15);
+        document.querySelector(".dragonLive").textContent = dragonLive;
+    } else {
+        live -= Math.ceil(Math.random() * 40);
+        dragonLive -= Math.ceil(Math.random() * 15);
+        document.querySelector(".dragonLive").textContent = dragonLive;
+    }
+    if (live < 500) {
+        wepons1 = "";
+    }
+    if (live <= 0) {
+        document.body.innerHTML = '<h1 style="text-align: center;">GAME OVER</h1>'
+    } else if (live < 150) {
+        document.querySelector(".result").innerHTML = "!!! ОПАСНОСТ ОТ ЗАГУБА !!!";
+    }
+
+    if (dragonLive <= 0) {
+        document.body.innerHTML = '<h1 style="text-align: center;">!!! VICTORY !!!</h1>'
+    }
+    document.querySelector(".dragonLive").textContent = dragonLive;
+    updateResurs();
+    danger();
+};
+
+//функция за победа над звяра
+function victoryAgenstBeast() {
+    if (counterFight < 5) {
+        alert("Бърза победа - печелиш 1 диамант");
+        counterFight = 0;
+        diamonds++;
+
+    } else {
+        counterFight = 0;
+    }
+    fight.style.display = "none";
+    winPic.style.display = "block"
+    victory++;
+    const combatButton = document.getElementById('combatButton');
+    const runButton = document.getElementById('runButton');
+    combatButton.disabled = true;
+    runButton.disabled = true;
+    let entryButton1 = document.getElementById("button1");
+    entryButton1.disabled = false;
+    let entrybuttno2 = document.getElementById("button2");
+    entrybuttno2.disabled = false;
+    let entrybuttno3 = document.getElementById("button3");
+    entrybuttno3.disabled = false;
+    let bardak = document.querySelector(".bardak");
+    bardak.disabled = false;
+    if (live < 200 && live > 100) {
+        wepons2 = "";
+    } else if (live < 100) {
+        wepons1 = "";
+        wepons2 = "";
+    }
+    updateResurs();
+
+}
+
+//сигнализатор за опасно малко живот
+function danger() {
+    const danger = document.getElementById("live");
+    if (live <= 20) {
+        danger.style.color = "red"
+     
+    } else {
+        danger.style.color = ""
+    }
+
+}
---
+++ script/script2.js
@@ -0,0 +1,339 @@
+//променлива за изгран залог
+let selectedGold = 0;
+
+// Проверяваме дали има запазени данни в Local Storage
+function loadData() {
+    if (localStorage.getItem('savedData')) {
+        // Ако има, възстановяваме променливите от Local Storage
+        let savedData = JSON.parse(localStorage.getItem('savedData'));
+        gold = savedData.gold;
+        live = savedData.live;
+        wepons1 = savedData.wepons1;
+        wepons2 = savedData.wepons2;
+        victory = savedData.victory;
+        beastWeapon = savedData.beastWeapon;
+        beastLive = savedData.beastLive;
+        counter = savedData.counter;
+        dragonLive = savedData.dragonLive;
+        diamonds = savedData.diamonds
+    }
+    updateResurs();
+}
+
+
+// Функция за запазване на променливите в Local Storage
+function saveData() {
+    if (fight.style.display === 'block' || killDragon.style.display === "block") {
+        alert('Не можете да създадете запис когато сте влязли в битка!')
+    } else {
+        let data = {
+            gold: gold,
+            live: live,
+            wepons1: wepons1,
+            wepons2: wepons2,
+            victory: victory,
+            counter: counter,
+            dragonLive: dragonLive,
+            diamonds: diamonds
+        };
+        localStorage.setItem('savedData', JSON.stringify(data));
+        alert("Играта беше запаметена");
+    }
+
+}
+//функция за ресет на играта
+function resset() {
+    localStorage.removeItem('savedData');
+    gold = 0;
+    live = 50;
+    wepons1 = "";
+    wepons2 = "";
+    victory = 0;
+    beastWeapon = '';
+    beastLive = 0;
+    counter = 0;
+    dragonLive = 400;
+    diamonds = 0;
+    updateResurs()
+    styleDisplay(false, false, false, false, false, true, false);
+    let entryButton1 = document.getElementById("button1");
+    entryButton1.disabled = false;
+    let entrybuttno2 = document.getElementById("button2");
+    entrybuttno2.disabled = false;
+}
+
+//Автоматична функция
+let otherButtons = document.querySelectorAll('button:not(#autoButton)'); // Избираме всички бутони, освен autoButton
+let intervalId;
+function auto(input, number) {
+
+    if (intervalId) {
+        // Ако вече имаме интервал, значи е стартирано автоматичното изпълнение
+        clearInterval(intervalId); // Спираме интервала
+        intervalId = null; // Нулираме променливата
+        autoButton.textContent = 'Автоматично копаене'; // Променяме текста на бутона
+        otherButtons.forEach(function (button) {
+            button.disabled = false;
+        });
+    } else {
+        let answer = confirm('Автоматично копаене , може да изчерпи целия Ви живот!! Сигурни ли сте ???')
+        if (answer) {
+            // Ако нямаме интервал, значи трябва да стартираме автоматичното изпълнение
+            intervalId = setInterval(input, number); // Стартираме интервал с 1 секунда
+            autoButton.textContent = 'Спри автоматичното копаене'; // Променяме текста на бутона
+            otherButtons.forEach(function (button) {
+                button.disabled = true;
+            });
+        };
+
+    };
+
+}
+
+//Автоматичен бутон
+let autoButton = document.getElementById('autoButton');
+autoButton.addEventListener('click', function () {
+
+    auto(deep, 500);
+});
+
+//функция за текущото време
+function time() {
+    let curHour;
+    let curMinute;
+    let curSeconds;
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+    if (hours < 10) {
+        curHour = `0${hours}`
+    } else {
+        curHour = hours
+    };
+    if (minutes < 10) {
+        curMinute = `0${minutes}`
+    } else {
+        curMinute = minutes
+    }
+    if (seconds < 10) {
+        curSeconds = `0${seconds}`
+    } else {
+        curSeconds = seconds;
+    }
+    document.querySelector(".hour").textContent = `The curent time is: ${curHour}:${curMinute}:${curSeconds} h`
+};
+setInterval(time, 1000);
+
+
+//функция за играта на ролетка
+let cheriPic1 = document.querySelector(".chery-pic1");
+let cheriPic2 = document.querySelector(".chery-pic2");
+let cheriPic3 = document.querySelector(".chery-pic3");
+let cheriPic4 = document.querySelector(".chery-pic4");
+let praskovaPic1 = document.querySelector(".praskova-pic1");
+let praskovaPic2 = document.querySelector(".praskova-pic2");
+let praskovaPic3 = document.querySelector(".praskova-pic3");
+let praskovaPic4 = document.querySelector(".praskova-pic4");
+let qgodapic1 = document.querySelector(".qgoda-pic1");
+let qgodapic2 = document.querySelector(".qgoda-pic2");
+let qgodapic3 = document.querySelector(".qgoda-pic3");
+let qgodapic4 = document.querySelector(".qgoda-pic4");
+let karti1 = document.querySelector(".karti1");
+let karti2 = document.querySelector(".karti2");
+let karti3 = document.querySelector(".karti3");
+let karti4 = document.querySelector(".karti4");
+let bananaPic1 = document.querySelector(".banana-pic1");
+let bananaPic2 = document.querySelector(".banana-pic2");
+let bananaPic3 = document.querySelector(".banana-pic3");
+let bananaPic4 = document.querySelector(".banana-pic4");
+
+//функция която извиква резултат на трите карти
+// функцията извиква резултата с различни аргументи от масива
+let firstResult = '';
+let secondResult = '';
+let thirdResult = '';
+let forthResult = '';
+
+function spin() {
+    if (selectedGold <= 0 || selectedGold === '') {
+        alert("Невалиден залог!")
+
+    } else if (selectedGold > 50) {
+        alert("Максимален залог 50 злато");
+
+    } else {
+        if (gold < selectedGold) {
+            alert("Нямате достатъчно злато,моля изберете друг залог!");
+
+            return;
+        }
+        gold -= selectedGold;
+        updateResurs();
+        cardVisibility();
+
+        // Създаване на масив с аргументи
+        let argumentsArray = ["first", "second", "third", "forth"];
+        // Извикване на функцията result() три пъти с различни аргументи и интервал от 1 секунда
+        let interval = setInterval(() => {
+            // Извикване на result() с текущия аргумент от масива
+            result(argumentsArray.shift());
+            // Спиране на изпълнението, ако няма повече аргументи
+            if (argumentsArray.length === 0) {
+                clearInterval(interval);
+            }
+        }, 600);
+        //проверка на резултата със закъснение от 3,4 сек
+        setTimeout(function () {
+
+            if (firstResult === "cheri" && secondResult === "cheri" && thirdResult === "cheri" && forthResult === "cheri") {
+                alert(`Череши ви носи печалба от ${selectedGold * 50}`);
+                gold += (selectedGold * 50);
+                updateResurs();
+            } else if (firstResult === "praskova" && secondResult === "praskova" && thirdResult === "praskova" && forthResult === "praskova") {
+                alert(`Праскови ви носи печалба от ${selectedGold * 100}`);
+                gold += (selectedGold * 100);
+                updateResurs();
+            } else if (firstResult === "qgoda" && secondResult === "qgoda" && thirdResult === "qgoda" && forthResult === "qgoda") {
+                alert(`Ягоди ви носи печалба от ${selectedGold * 150}`);
+                gold += (selectedGold * 150);
+                updateResurs();
+            } else if (firstResult === "banana" && secondResult === "banana" && thirdResult === "banana" && forthResult === "banana") {
+                alert(`Банани ви носи печалба от ${selectedGold * 200}`);
+                gold += (selectedGold * 200);
+                updateResurs();
+            }
+
+            if(firstResult === secondResult && thirdResult === forthResult){
+                alert(`Два чифта ви носи печалба от ${selectedGold * 5}`);
+                gold += (selectedGold * 5);
+                updateResurs();
+            }
+            
+        }, 2500);
+    }
+    
+}
+
+
+//Резултат от рулетка
+function result(input) {
+    let rotate = Math.random()
+    switch (input) {
+        case "first":
+            karti1.style.display = "none";
+            if (rotate <= 1 / 4) {
+                cheriPic1.style.display = "block";
+                firstResult = "cheri";
+            } else if (rotate >= 1 / 4 && rotate < 2 / 4) {
+                praskovaPic1.style.display = "block";
+                firstResult = "praskova";
+            } else if (rotate >= 2 / 4 && rotate < 3 / 4) {
+                qgodapic1.style.display = "block"
+                firstResult = "qgoda";
+            } else {
+                bananaPic1.style.display = "block"
+                firstResult = "banana"
+            }
+            break;
+
+        case "second":
+            karti2.style.display = "none";
+            if (rotate <= 1 / 4) {
+                cheriPic2.style.display = "block";
+                secondResult = "cheri";
+            } else if (rotate >= 1 / 4 && rotate < 2 / 4) {
+                praskovaPic2.style.display = "block";
+                secondResult = "praskova";
+            } else if (rotate >= 2 / 4 && rotate < 3 / 4) {
+                qgodapic2.style.display = "block"
+                secondResult = "qgoda";
+            } else {
+                bananaPic2.style.display = "block"
+                secondResult = "banana"
+            }
+            break;
+
+        case "third":
+            karti3.style.display = "none";
+            if (rotate <= 1 / 4) {
+                cheriPic3.style.display = "block";
+                thirdResult = "cheri";
+            } else if (rotate >= 1 / 4 && rotate < 2 / 4) {
+                praskovaPic3.style.display = "block";
+                thirdResult = "praskova";
+            } else if (rotate >= 2 / 4 && rotate < 3 / 4) {
+                qgodapic3.style.display = "block"
+                thirdResult = "qgoda";
+            } else {
+                bananaPic3.style.display = "block"
+                thirdResult = "banana"
+            }
+            break;
+
+        case "forth":
+            karti4.style.display = "none";
+            if (rotate <= 1 / 4) {
+                cheriPic4.style.display = "block";
+                forthResult = "cheri";
+            } else if (rotate >= 1 / 4 && rotate < 2 / 4) {
+                praskovaPic4.style.display = "block";
+                forthResult = "praskova";
+            } else if (rotate >= 2 / 4 && rotate < 3 / 4) {
+                qgodapic4.style.display = "block"
+                forthResult = "qgoda";
+            } else {
+                bananaPic4.style.display = "block"
+                forthResult = "banana"
+            }
+            break;
+    }
+    console.log(`${firstResult}, ${secondResult},${thirdResult}, ${forthResult}`);
+}
+
+//Функция за видимоста на картите
+function cardVisibility() {
+    karti1.style.display = "block"
+    karti2.style.display = "block"
+    karti3.style.display = "block"
+    karti4.style.display = "block"
+    cheriPic1.style.display = "none";
+    cheriPic2.style.display = "none";
+    cheriPic3.style.display = "none";
+    cheriPic4.style.display = "none";
+    praskovaPic1.style.display = "none";
+    praskovaPic2.style.display = "none";
+    praskovaPic3.style.display = "none";
+    praskovaPic4.style.display = "none";
+    qgodapic1.style.display = "none"
+    qgodapic2.style.display = "none"
+    qgodapic3.style.display = "none"
+    qgodapic4.style.display = "none"
+    bananaPic1.style.display = "none"
+    bananaPic2.style.display = "none"
+    bananaPic3.style.display = "none"
+    bananaPic4.style.display = "none"
+};
+
+
+
+//Вход в странноприемница
+document.querySelector(".bardak").addEventListener("click", function () {
+    let hours = new Date().getHours();
+
+    if (hours >= 6 && hours < 10) {
+        alert("Странноприемницата не работи от 6 до 10 h");
+    } else {
+        styleDisplay(false, false, false, false, false, false, true);
+        updateResurs();
+        danger();
+        cardVisibility();
+    }
+})
+
+//потвърждаване на избран залог
+document.getElementById("confirm-button").addEventListener("click", function () {
+
+    const selested = document.getElementById("zalog");
+    selectedGold = selested.value;
+
+});
