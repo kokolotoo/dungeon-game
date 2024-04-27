3b4a216dfd5edd2cb2c72f60143f3372dda4a118
Nikolai <chapi.kaica@gmail.com>
2024-04-28 01:23:52 +0300

    test

--- script/strangers.js
+++ script/strangers.js
@@ -11,7 +11,9 @@ let selectedGold = 0;
 let beastLive = 0;
 let barTalk = 0;
 
-
+if (window.opener && !window.opener.closed) {
+    window.opener.close();
+}
 //функция за показване на ресурсите
 function updateResurs() {
     document.getElementById("pokaji-zlato").textContent = gold;
